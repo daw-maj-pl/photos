25befe37b2a283c840fefe6afe2a903a56d1adc0
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2021-07-12 20:12:32 +0200

    Initial commit

---
+++ src/app/photos.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface UnsplashResponse {
+  urls: {
+    regular: string;
+  };
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PhotosService {
+  constructor(private http: HttpClient) {}
+
+  getPhoto() {
+    return this.http.get<UnsplashResponse>(
+      'https://api.unsplash.com/photos/random',
+      {
+        headers: {
+          Authorization:
+            'Client-ID w1eAKYxCF8ebDPCJ9VqmlBe1qEb_b3caNm0LeaPcFVk',
+        },
+      }
+    );
+  }
+}
